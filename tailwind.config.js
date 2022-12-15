cb83c326c89cc2e8a27ad349d949c6a94dedb64f
Nick Ezekiel <nickezekiel@ignislab.com>
2022-12-15 15:08:55 +0100

    conf tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
+  darkMode: "class",
+  corePlugins: {
+    preflight: false,
+  },
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
